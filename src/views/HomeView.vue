5ccae917d81676980da13f72a358286af8cb600b
tomcat <cooge123@gmail.com>
2025-08-26 10:21:25 +0800

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -21,40 +21,80 @@ const options = {
 
 const getMD = (port: number | Number | string | undefined) => {
 
-  const md = '\n' +
-    '**GET Request Example**:\n' +
-    '\n' +
-    '```powershell\n' +
-    'curl \'http://127.0.0.1:'+port+'/sendMail?subject=test&token={{token}}&content=this%20is%20a%20test&recipients=aaa@mail.com,bbb@mail.com\'\n' +
-    '```\n' +
-    '\n' +
-    '**POST Request Example**:\n' +
-    '\n' +
-    '```powershell\n' +
-    'curl -X POST \'http://127.0.0.1:'+port+'/sendMail\' \\\n' +
-    '--header \'Content-Type: application/x-www-form-urlencoded\' \\\n' +
-    '--data-urlencode \'token={{token}}\' \\\n' +
-    '--data-urlencode \'content=this%20is%20a%20test\'\n' +
-    '```'
-    +'\n**Example of Sending an Email with Attachments**:\n' +
-    '\n' +
-    '```powershell\n' +
-    'curl -X POST \'http://127.0.0.1:'+port+'/sendMail\' \\\n' +
-    '--form \'files=@"/111111.txt"\' \\\n' +
-    '--form \'files=@"/22222222222222.txt"\' \\\n' +
-    '--form \'token={{token}}"\' \\\n' +
-    '--form \'content=1212\'\n' +
-    '```\n'+
-    '**POST submission json format example**:\n' +
-    '```powershell\n' +
-    'curl -X POST \'http://127.0.0.1:'+port+'/sendMail\' \\\n' +
-    '--header \'Content-Type: application/json\' \\\n' +
-    '--data \'{\n' +
-    '"token": "{{token}}",\n' +
-    '"content":"this is a test",\n' +
-    '"recipients":["aaa@mail.com","bbb@mail.com"]\n' +
-    '}\'\n' +
-    '```'
+  // const md = '\n' +
+  //   '**GET Request Example**:\n' +
+  //   '\n' +
+  //   '```powershell\n' +
+  //   'curl \'http://127.0.0.1:'+port+'/sendMail?subject=test&token={{token}}&content=this%20is%20a%20test&recipients=aaa@mail.com,bbb@mail.com\'\n' +
+  //   '```\n' +
+  //   '\n' +
+  //   '**POST Request Example**:\n' +
+  //   '\n' +
+  //   '```powershell\n' +
+  //   'curl -X POST \'http://127.0.0.1:'+port+'/sendMail\' \\\n' +
+  //   '--header \'Content-Type: application/x-www-form-urlencoded\' \\\n' +
+  //   '--data-urlencode \'token={{token}}\' \\\n' +
+  //   '--data-urlencode \'content=this%20is%20a%20test\'\n' +
+  //   '```'
+  //   +'\n**Example of Sending an Email with Attachments**:\n' +
+  //   '\n' +
+  //   '```powershell\n' +
+  //   'curl -X POST \'http://127.0.0.1:'+port+'/sendMail\' \\\n' +
+  //   '--form \'files=@"/111111.txt"\' \\\n' +
+  //   '--form \'files=@"/22222222222222.txt"\' \\\n' +
+  //   '--form \'token={{token}}"\' \\\n' +
+  //   '--form \'content=1212\'\n' +
+  //   '```\n'+
+  //   '**POST submission json format example**:\n' +
+  //   '```powershell\n' +
+  //   'curl -X POST \'http://127.0.0.1:'+port+'/sendMail\' \\\n' +
+  //   '--header \'Content-Type: application/json\' \\\n' +
+  //   '--data \'{\n' +
+  //   '"token": "{{token}}",\n' +
+  //   '"content":"this is a test",\n' +
+  //   '"recipients":["aaa@mail.com","bbb@mail.com"]\n' +
+  //   '}\'\n' +
+  //   '```'
+  const md = `
+
+
+**Example of JSON format**
+
+\`\`\`bash
+curl -X POST 'http://127.0.0.1:${port}/sendMail' \\
+--header 'Content-Type: application/json' \\
+--data-raw '{
+  "token": "{{token}}",
+  "subject": "test",
+  "content": "this is a test",
+  "recipients": ["ops@example.com"],
+  "files": [
+    {
+      "name": "alert.log",
+      "data": "{{base64_content}}"
+    }
+  ]
+}'
+\`\`\`
+
+**Example of form submission**
+
+\`\`\`bash
+curl -X POST 'http://127.0.0.1:${port}/sendMail' \\
+--form 'token={{token}}' \\
+--form 'subject=test' \\
+--form 'content=this is a test' \\
+--form 'recipients=finance@example.com,sales@example.com' \\
+--form 'files=@"/data/reports/sales.pdf"' \\
+--form 'files=@"/data/reports/expenses.xlsx"'
+\`\`\`
+**Example of get request**
+\`\`\`bash
+curl 'http://127.0.0.1:${port}/sendMail?token={{token}}&subject=test&content=this%20is%20a%20test&recipients=aaa@mail.com,bbb@mail.com'
+\`\`\`
+
+
+  `;
   return md
 
 }
